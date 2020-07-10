78cad3ae3fd6831c69a7582ec3e7b0c564b79c28
jrfarin <pepfarin@gmail.com>
2020-07-10 15:38:16 +0200

    Middlewares: Morgan(logs), Multer(images)

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,25 @@
-const express = require('express');
 
+const express = require('express');
+const morgan = require('morgan');
+const multer = require('multer');
+const path = require('path');
 
 
 // Inits
 const app = express();
+
+
+// Middlewares 
+app.use(morgan('dev'));
+const storage = multer.diskStorage({
+    destination: path.join(__dirname, 'public/uploads'),
+    filename(req, file, cb) {
+        cb(null, new Date().getTime() + path.extname(file.originalname));
+    }
+})
+app.use(multer({storage}).single('image'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
 
 // Settings
